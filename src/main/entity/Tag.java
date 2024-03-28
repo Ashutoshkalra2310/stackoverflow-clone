120d22c38004f8dccf6f1c5fc27ae6ad40554cd0
Madhavi Panakala <madhavi.panakala.27.2@mountblue.tech>
2024-03-28 06:18:59 +0000

    Add Tag entity

---
+++ src/main/entity/Tag.java
@@ -0,0 +1,17 @@
+package io.mountblue.stackoverflowclone.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="tag")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String name;
+    String description;
+}
