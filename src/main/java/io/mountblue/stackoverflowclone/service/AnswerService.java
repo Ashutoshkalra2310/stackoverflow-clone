92f721072a67efc403cada21a5238f6d22537300
Madhavi Panakala <madhavi.panakala.27.2@mountblue.tech>
2024-04-01 10:24:42 +0000

    Update AnswerService.java

--- src/main/java/io/mountblue/stackoverflowclone/service/AnswerService.java
+++ src/main/java/io/mountblue/stackoverflowclone/service/AnswerService.java
@@ -1,5 +1,8 @@
 package io.mountblue.stackoverflowclone.service;
 
 public interface AnswerService {
+    public void saveAnswer(Answer answer);
+    public void updateAnswer(Long id,Answer updatedAnswer);
+    public void deleteAnswer(Long id);
 
 }
