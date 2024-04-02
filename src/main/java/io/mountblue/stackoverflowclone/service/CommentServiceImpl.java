a1346c2f3c847c62b6ae13022679a94539fa9a74
Nivesh Soni <nivesh.soni.27.2@mountblue.tech>
2024-04-03 00:57:38 +0530

    completed update question comment

--- src/main/java/io/mountblue/stackoverflowclone/service/CommentServiceImpl.java
+++ src/main/java/io/mountblue/stackoverflowclone/service/CommentServiceImpl.java
@@ -3,7 +3,10 @@ package io.mountblue.stackoverflowclone.service;
 import io.mountblue.stackoverflowclone.entity.Answer;
 import io.mountblue.stackoverflowclone.entity.Comment;
 import io.mountblue.stackoverflowclone.entity.Question;
+import io.mountblue.stackoverflowclone.entity.User;
 import io.mountblue.stackoverflowclone.repository.CommentRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,11 +19,13 @@ public class CommentServiceImpl implements CommentService{
     private final CommentRepository commentRepository;
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
-    public CommentServiceImpl(CommentRepository commentRepository, QuestionService questionService, AnswerService answerService) {
+    public CommentServiceImpl(CommentRepository commentRepository, QuestionService questionService, AnswerService answerService, UserService userService) {
         this.commentRepository = commentRepository;
         this.questionService = questionService;
         this.answerService = answerService;
+        this.userService = userService;
     }
 
     @Override
@@ -34,16 +39,21 @@ public class CommentServiceImpl implements CommentService{
         comment.setUpdatedAt(formattedDateTime);
         question.setComments(comments);
         comment.setQuestion(question);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findByEmail(authentication.getName());
+        comment.setUser(user);
         questionService.save(question);
     }
 
     @Override
     public void updateQuestionComment(Comment comment) {
+        Comment existingComment = commentRepository.findById(comment.getId()).get();
         LocalDateTime localDateTime = LocalDateTime.now();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formattedDateTime = localDateTime.format(dateTimeFormatter);
-        comment.setUpdatedAt(formattedDateTime);
-        commentRepository.save(comment);
+        existingComment.setUpdatedAt(formattedDateTime);
+        existingComment.setComment(comment.getComment());
+        commentRepository.save(existingComment);
     }
 
     @Override
