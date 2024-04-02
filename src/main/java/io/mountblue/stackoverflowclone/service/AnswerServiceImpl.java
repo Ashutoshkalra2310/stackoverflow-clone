c659cd4f7774ebd99a615298825e25429fe01161
Madhavi Panakala <madhavi.panakala.27.2@mountblue.tech>
2024-04-02 05:02:30 +0000

    Add new file AnswerServiceImpl

---
+++ src/main/java/io/mountblue/stackoverflowclone/service/AnswerServiceImpl.java
@@ -0,0 +1,32 @@
+package io.mountblue.stackoverflowclone.service;
+
+import io.mountblue.stackoverflowclone.entity.Answer;
+import io.mountblue.stackoverflowclone.repository.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class AnswerServiceImpl implements AnswerService{
+    private final AnswerRepository answerRepository;
+
+    public AnswerServiceImpl(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+    public void saveAnswer(Answer answer) {
+        answerRepository.save(answer);
+    }
+    public void updateAnswer(Long id,Answer updatedAnswer) {
+        Answer prevAnswer=answerRepository.findById(id).get();
+        prevAnswer.setContent(updatedAnswer.getContent());
+        prevAnswer.setUpdatedAt(LocalDateTime.now());
+        prevAnswer.setComments(updatedAnswer.getComments());
+        answerRepository.save(prevAnswer);
+
+    }
+
+    public void deleteAnswer(Long id) {
+        answerRepository.deleteById(id);
+    }
+}
