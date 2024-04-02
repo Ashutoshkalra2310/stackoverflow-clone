1f9147733398e74f3dcea6a427c4c1b363ae09c1
Madhavi Panakala <madhavi.panakala.27.2@mountblue.tech>
2024-04-02 12:00:50 +0000

    Add new file UserService

---
+++ src/main/java/io/mountblue/stackoverflowclone/service/UserService.java
@@ -0,0 +1,9 @@
+package io.mountblue.stackoverflowclone.service;
+
+import io.mountblue.stackoverflowclone.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+}
